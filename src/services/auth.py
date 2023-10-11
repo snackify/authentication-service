fdff49bf25b22e6acea11348cee1db4455be6173
Grisha Malanchuk <grishamalanchuk@gmail.com>
2023-10-11 14:47:57 +0300

    validators

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,24 +1,19 @@
-from fastapi import HTTPException
-from sqlalchemy.exc import IntegrityError
-
 from ..repositories.auth import AuthRepository
 from ..utils.hash_password import HashPassword
+from .validators.auth import AuthValidator
 
 
 class AuthService:
     def __init__(self) -> None:
         self.auth_repository = AuthRepository()
         self.hash_password = HashPassword()
-        # self.auth_validator = AuthValidator()
+        self.auth_validator = AuthValidator()
 
     async def registration(self, user: dict):
-        # await self.auth_validator.all_validators(username=user['username'], email=user['email'])
+        await self.auth_validator.username_email_validators(username=user["username"], email=user["email"])
 
         user["hashed_password"] = self.hash_password.create_hash(user.pop("password"))
 
-        try:
-            res = await self.auth_repository.add_one(user)
-        except IntegrityError:  # todo выкинуть валидацию в миксин
-            raise HTTPException(detail="duplicate email or username", status_code=409)
+        res = await self.auth_repository.add_one(user)
 
         return res
--- src/utils/repository.py
+++ src/utils/repository.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import NoReturn
 
+from sqlalchemy import select
+
 from ..database.session import async_session
 
 
@@ -22,3 +24,13 @@ class SQLAlchemyRepository(AbstractRepository):
             await session.commit()
 
             return model_obj
+
+    async def get_one_by_email(self, email: str):
+        async with async_session() as session:
+            query = select(self.model).where(self.model.email == email)
+            return (await session.execute(query)).scalars().first()
+
+    async def get_one_by_username(self, username: str):
+        async with async_session() as session:
+            query = select(self.model).where(self.model.username == username)
+            return (await session.execute(query)).scalars().first()
